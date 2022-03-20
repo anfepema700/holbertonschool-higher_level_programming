3ed6447f41d4a28ff6ff8627bff9335d964cd1d9
anfepema <anfepema700>
2022-03-20 07:01:58 -0700

    9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,5 @@
+--create a table second table and her attributes
+USE hbtn_0c_0
+GO
+CREATE TABLE IF NOT EXISTS second_table (id INT, VARCHAR(256), score INT);
+INSERT INTO second_table (id,name,score) VALUES (1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8);
