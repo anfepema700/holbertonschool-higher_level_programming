830ff9f2f1c77153ae0e26306d378ccdc66178d6
anfepema <anfepema700>
2022-03-20 07:39:58 -0700

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 --show all records that name be different to empty order score and name
 USE hbtn_0c_0
 GO
-SELECT score, name FROM second_table WHERE name 1='' ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
