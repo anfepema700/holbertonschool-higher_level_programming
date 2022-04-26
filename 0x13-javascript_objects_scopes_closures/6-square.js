d3ed342d563855f42234bc04f70fc591aa67bf79
anfepema700 <anfepema700@gmail.com>
2022-04-26 15:41:08 -0700

    6-square.js changes in function

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,13 @@
 const Square1 = require('./5-square.js');
 
 module.exports = class Square extends Square1 {
-  charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.width; i++) {
-        console.log(c.repeat(this.height));
-      }
-    }
+	  charPrint (c) {
+		      if (c === undefined) {
+			            this.print();
+			          } else {
+					        for (let i = 0; i < this.width; i++) {
+							        console.log(c.repeat(this.height));
+							      }
+					      }
+		    }
 };
