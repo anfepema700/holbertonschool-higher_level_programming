8b2a314e6408f20caa9a624ef9ce5b784541397e
anfepema700 <anfepema700@github.com>
2022-05-01 19:56:12 -0500

    8-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
 //fetches the character name from URL
-$.get("https://swapi-api.hbtn.io/api/people/5/?format=json", function(resp){
-	$("#character").text(resp.name);
+$.get("https://swapi-api.hbtn.io/api/people/5/?format=json", function(data){
+	$("#character").text(data.name);
 });
