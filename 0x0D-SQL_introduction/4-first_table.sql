b59ebfe9ac20f0242fa65a153b25b7fe0c63ff7a
anfepema <anfepema700>
2022-05-10 08:19:27 -0700

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
---Create table first table description 
---atributes id, name
+-- Create table first table description 
+-- atributes id, name
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0
 CREATE TABLE IF NOT EXISTS first_table (id INT,name VARCHAR(256));
