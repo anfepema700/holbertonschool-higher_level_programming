d5e725c146a7f021adc746326ca538ff246f31a5
anfepema <anfepema700>
2022-05-10 08:39:18 -0700

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 --Show the records that score be >=10 in the second table
 
-SELECT score, name FROM second_table WHERE score >= 10 DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
