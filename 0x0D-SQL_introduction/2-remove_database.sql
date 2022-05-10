c1a0695697ff4af91704cac7bc330e57de7df12f
anfepema <anfepema700>
2022-05-10 08:14:53 -0700

    2-remove_database.sql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,4 @@
---Delete database if exists hbn0c_0
-DROP DATABASE IF EXISTS hbtn_0c_0
+-- Delete database if exists hbn0c_0
+-- if exists elsed script should not fail
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
