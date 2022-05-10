8f201f294a8197a1f2ad09fd93388248e71d36cf
anfepema <anfepema700>
2022-05-10 08:34:25 -0700

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,10 @@
 -- create a table second table and her attributes
 
-CREATE TABLE IF NOT EXISTS second_table (id INT, VARCHAR(256), score INT);
-INSERT INTO second_table (id,name,score) VALUES (1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8);
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+	name VARCHAR(256),
+	score INT);
+	INSERT INTO second_table (id, name, score)
+	VALUES (1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
